1d1f96585ad681b04f4ef44e787e9a88c28ba67c
RufanNiyazli <developerx73@gmail.com>
2025-07-22 21:29:57 +0400

    configure webSocket

---
+++ src/main/java/com/project/websocketchatapp/chat/ChatController.java
@@ -0,0 +1,4 @@
+package com.project.websocketchatapp.chat;
+
+public class ChatController {
+}
---
+++ src/main/java/com/project/websocketchatapp/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.project.websocketchatapp.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ChatMessage {
+    private MessageType messageType;
+    private String content;
+    private String sender;
+}
